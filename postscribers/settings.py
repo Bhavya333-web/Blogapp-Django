6681ba72b8162aae62844007a0b2e6407c0b229c
Bhavya Mittal <bhavyamittal575@gmail.com>
2024-10-27 01:16:44 +0530

    Blogapp

--- postscribers/settings.py
+++ postscribers/settings.py
@@ -150,7 +150,7 @@ STATIC_ROOT = (BASE_DIR / 'asset')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'postscribers@gmail.com'
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_HOST_USER = 'bhavyamittal575@gmail.com'
+EMAIL_HOST_PASSWORD = 'hlosejggtncnqbfu'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
